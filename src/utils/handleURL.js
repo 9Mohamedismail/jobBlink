5393ffa64743764e3a2f504ae812bef6f5629a98
9Mohamedismail <9Mohamedismail@gmail.com>
2025-05-21 22:12:18 -0400

    updated handleURL with Netifly

--- src/utils/handleURL.js
+++ src/utils/handleURL.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 export const handleURL = async (encodedUrl) => {
+  const API = import.meta.env.VITE_API_BASE_URL;
+  const { data } = await axios.get(`${API}/job?url=${encodedUrl}`);
+
+  /* If you are using LOCALHOST then use this instead:
   const { data } = await axios.get(
     `http://localhost:5000/api/job?url=${encodedUrl}`,
-  );
+  );*/
   return { ...data };
 };
